c6205e3fdef65ea18410da499ff6416d3d6a5266
Tyler Watson <leggettwatson.tyler@gmail.com>
2018-11-07 22:48:25 -0800

    Added es6 stack and queue

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,32 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+  	this.storage = {};
+  	this.capacity = 0
   }
 
+  push(value) {
+     this.storage[this.capacity] = value;
+     this.capacity++;
+  }
+
+  pop () {
+  var temp;
+	  if (this.capacity > 0) {
+	    temp = this.storage[this.capacity - 1];
+	    this.capacity--;
+	    return temp;
+	  } else {
+	    return 0;
+	  }
+ }
+
+ size() {
+     return this.capacity;
+ }
 }
+
+
+
+
+

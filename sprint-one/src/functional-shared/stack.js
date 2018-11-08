86cc58127b9eeb7facc4a0a56f89749af10b9ac2
Tyler Watson <leggettwatson.tyler@gmail.com>
2018-11-07 20:42:06 -0800

    Added functionality for functional-shared stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,35 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {}
+	extend(someInstance, stackMethods);
+	storage = {};
+	size = 0;
+  	front = 0;
+  	back = 0;
+  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
+  // but try not not reference your old code in writing the new style.
+  	return someInstance;
 };
 
 var stackMethods = {};
 
+   stackMethods.push = function(value) {
+     storage[size] = value;
+     size++;
+   };
 
+   stackMethods.pop = function() {
+    var temp;
+     if (size > 0) {
+       temp = storage[size - 1];
+       size--;
+       return temp;
+     } else{
+      return 0;
+     }
+   };
+
+   stackMethods.size = function() {
+     return size;
+   };

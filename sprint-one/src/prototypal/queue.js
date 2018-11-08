8217cba6e6c1273b415a31c20e0c6507f73468d9
Tyler Watson <leggettwatson.tyler@gmail.com>
2018-11-07 22:16:17 -0800

    Added functionality for prototypal stack and queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,31 @@
 var Queue = function() {
+	var someInstance = Object.create(queueMethods);
+	someInstance.storage = {};
+  	someInstance.front = 0;
+  	someInstance.back = 0;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  	return someInstance;
 };
 
-var queueMethods = {};
+queueMethods = {
+	enqueue : function(value) {
+  	this.storage[this.back] = value;
+  	this.back++;
+	},
+
+	dequeue: function() {
+  	if (this.back > this.front) {
+    	var temp = this.storage[this.front];
+    	delete this.storage[this.front];
+    	this.front++;
+    	return temp;
+  		}
+	},
+	size: function() {
+  	return this.back - this.front;
+	}
+};
+
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,32 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+	var someInstance = Object.create(stackMethods);
+	someInstance.storage = {};
+  	someInstance.capacity = 0
+  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
+  // but try not not reference your old code in writing the new style.
+  	return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+	push: function(value) {
+     this.storage[this.capacity] = value;
+     this.capacity++;
+   },
 
+   pop: function() {
+    var temp;
+     if (this.capacity > 0) {
+       temp = this.storage[this.capacity - 1];
+       this.capacity--;
+       return temp;
+     } else{
+       return 0;
+     }
+   },
 
+   size: function() {
+     return this.capacity;
+   }
+};

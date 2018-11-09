bc7604afc560d5e8c7d87af89bcbb0abb83259af
TLW914 <leggettwatson.tyler@gmail.com>
2018-11-09 11:26:01 -0800

    Implemented hashTable insert and retrieve

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,16 +1,21 @@
 
 
 var HashTable = function() {
+  // var newHash = Object.create(HashTable.prototype)
   this._limit = 8;
-  this._storage = LimitedArray(this._limit);
+  this._storage = LimitedArray(this._limit); //this = newHash
+  //return newHash;
 };
 
 HashTable.prototype.insert = function(k, v) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  var index = getIndexBelowMaxForKey(k, this._limit); // sets index to random value via helper function this.limit = the limit of our newHash table
+  console.log(k)
+  this._storage.set(index, v)
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index)
 };
 
 HashTable.prototype.remove = function(k) {
